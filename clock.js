f203311dc9f54b7798b1c44aa1ff2b9675331f5c
ToBeNew <crebiz76@gmail.com>
2020-03-19 00:47:25 +0900

    update from repl.it

--- clock.js
+++ clock.js
@@ -1,6 +1,11 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
 
+const stopwatchContainer = document.querySelector(".js-stopwatch");
+const stopwatchTitle = stopwatchContainer.querySelector("h1");
+
+
+
 function getTime(){
     const date = new Date();
     const minutes = date.getMinutes();
@@ -9,6 +14,33 @@ function getTime(){
     clockTitle.innerText = `${hours<10?`0${hours}`:hours}:${
                               minutes<10?`0${minutes}`:minutes}:${
                               seconds<10?`0${seconds}`:seconds}`;
+	
+	const MaxHour = 17;
+	const MaxMinute = 0;
+	const MaxSecond = 0;
+	var flag = false;
+	
+	var gapHour = 0;
+	var gapMinute = 0;
+	var gapSecond = 0;
+	
+	if((hours >= MaxHour) && (minutes >= MaxMinute) && (seconds >= MaxSecond))
+	{
+		flag = true;
+		gapHour = hours - MaxHour;
+		gapMinute = minutes - MaxMinute;
+		gapSecond = seconds - MaxSecond;
+	}
+	else
+	{
+		flag = false;
+		gapHour = MaxHour - hours;
+		gapMinute = MaxMinute - minutes;
+		gapSecond = MaxSecond - seconds;
+	}
+	stopwatchTitle.innerText = `${flag == true?`+`:`-`}${gapHour<10?`0${gapHour}`:gapHour}:${
+                                  gapMinute<10?`0${gapMinute}`:gapMinute}:${
+                                  gapSecond<10?`0${gapSecond}`:gapSecond}`;
 }
 function init(){
     getTime();
