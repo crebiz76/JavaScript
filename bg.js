75a9c0af09931973ab3fbd15e21dc05c6d78b586
ToBeNew <crebiz76@gmail.com>
2020-06-05 00:59:22 +0900

    ch.30

---
+++ bg.js
@@ -0,0 +1,28 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 5;
+
+function handleImgLoad(){
+    console.log('finished loading');
+}
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`;
+    // image.addEventListener("loadend", handleImgLoad());
+    image.classList.add("bgImage");
+    body.appendChild(image);
+}
+
+function genRandom(){
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init(){
+    const randomNumber = genRandom();
+    // console.log(randomNumber);
+    paintImage(randomNumber);
+}
+
+init();
