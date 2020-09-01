b5395fd651ecb8a7650049a8ae52c119a87d5724
ToBeNew <crebiz76@gmail.com>
2020-09-02 01:38:36 +0900

    test

--- bg.js
+++ bg.js
@@ -8,9 +8,10 @@ function handleImgLoad(){
 
 function paintImage(imgNumber){
     const image = new Image();
-    //image.src = `https://github.com/crebiz76/JavaScript/tree/master/images/${imgNumber + 1}.jpg`;
-    //image.src = `./images/${imgNumber + 1}.jpg`;
-    image.src = `./images/${imgNumber + 1}.jpg`;
+    //image.src = `https://github.com/crebiz76/JavaScript/blob/master/images/${imgNumber + 1}.jpg`;
+    image.src = `/images/${imgNumber + 1}.jpg`;
+    //image.src = `./tree/master/images/${imgNumber + 1}.jpg`;
+    //image.src = `https://crebiz76.github.io/JavaScript/images/${imgNumber + 1}.jpg`;
     // image.addEventListener("loadend", handleImgLoad());
     image.classList.add("bgImage");
     body.appendChild(image);
